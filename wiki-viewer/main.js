5afd994ca898bb0e2324bf510b10ffcf530eca83
Jared Caraway <jared.caraway@gmail.com>
2018-04-21 01:11:21 -0500

    (wiki-viewer) Add event listener to detect when enter is pressed in search box

--- wiki-viewer/main.js
+++ wiki-viewer/main.js
@@ -1,6 +1,12 @@
 const random = document.querySelector("#random");
+const search = document.querySelector("#search");
 
 random.addEventListener("click", getRandomPage);
+search.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        alert("Enter key pressed");
+    }
+});
 
 function getRandomPage() {
     window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
